7c989827269e2ac318805278ed641689a34cc228
wangdetash <wangdetash@gmail.com>
2017-09-23 10:31:57 +0530

    program to use two adc and transmit through uart and incorporated temperature sensing using lm35 and ldr

--- ldr.c
+++ ldr.c
@@ -1,12 +1,12 @@
-
+//program to use two adc and transmit through uart
 #include <LPC214X.H>
 void transmit(char *);
 void recieve(char *);
 void main()
 {
 char a[10];
-int x,y;
-PINSEL0=0X00000005;
+int ldr_out,lm35_out,temp;
+PINSEL0=0X00000005;	 	  
 IODIR0=0X000000001;
 U0LCR=0X83;
 U0DLL=97;
@@ -16,11 +16,22 @@ while(1)
  AD0CR=0X01200008;
  AD1CR=0X01200008;
  while(!(AD0GDR)&(80000000));
- x=((AD0GDR>>6)&(0X03FF));
+ ldr_out=((AD0GDR>>6)&(0X03FF));
  while(!(AD1GDR)&(80000000));
- y=((AD1GDR>>6)&(0X03FF));
- sprintf(a,"%d\t%d\n",x,y);  
- transmit(a);
+ lm35_out=((AD1GDR>>6)&(0X03FF));
+ temp=((lm35_out*100)/1024);
+ sprintf(a,"temperature = %dDegree C \t %d\t",temp,lm35_out);
+ transmit(a); 
+ if(ldr_out<512)
+ {
+   IOSET0=0X80000000;
+   transmit("lights on\t");
+ }
+ else 
+ {
+ IOCLR0=0X80000000;
+ transmit("lights off\t");
+ } 
 }
 }
 
