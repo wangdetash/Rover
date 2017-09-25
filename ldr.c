60d3f49173cdfeef342bc7e530f38b0d9bc27c27
wangdetash <wangdetash@gmail.com>
2017-09-25 10:37:07 +0530

    program to use two adc and transmit through uart and incorporated temperature sensing using lm35,ldr and rtc

--- ldr.c
+++ ldr.c
@@ -1,4 +1,4 @@
-//program to use two adc and transmit through uart and incorporated temperature sensing using lm35 and ldr
+//program to use two adc and transmit through uart and incorporated temperature sensing using lm35,ldr and rtc
 #include <LPC214X.H>
 void transmit(char *);
 void forward();
@@ -12,7 +12,7 @@ char control_data;
 void main()
 {
 char a[20];
-int ldr_out,lm35_out,temp;
+int ldr_out,lm35_out,temp,date,month,year,hour,min,sec;
 PINSEL0=0X00000005;	 	  
 IODIR0=0X000000001;
 PINSEL2=0X00000000;
@@ -20,47 +20,65 @@ IODIR1=0X0000003F;
 U0LCR=0X83;
 U0DLL=97;
 U0LCR=0X03;
+
+CCR =0X11;
+DOM=23;
+MONTH=10;
+YEAR=2107;
+HOUR=22;
+MIN=10;
+SEC=40;
+
 while(1)
 {
 	while(!(U0LSR&1<<0));
-		control_data=U0RBR;
-		U0THR=control_data;
-		while(!(U0LSR&1<<5));
+	control_data=U0RBR;
+	U0THR=control_data;
+	while(!(U0LSR&1<<5));
 	 
-        if(control_data=='w')
+    if(control_data=='w')
 	      {
 	      	forward();
 	      	transmit("FORWARD\n");
 	      }
-		else if(control_data=='s')
+	else if(control_data=='s')
 		  {
 		 	stop();
 		 	transmit("STOP\n");
 		  }
-		else if(control_data=='x')
+	else if(control_data=='x')
 	      {
 	       	backward();
 	       	transmit("BACKWARD\n");
 	      }
-		else if(control_data=='a')
+	else if(control_data=='a')
 	      {
 	       	left();
 	       	transmit("LEFT\n");
 	      }
-		else if(control_data=='d')
+	else if(control_data=='d')
 	      {
 	       	right();
 	       	transmit("RIGHT\n");
 	      }	
- 		AD0CR=0X01200008;
- 		AD1CR=0X01200008;
- 		while(!(AD0GDR)&(80000000));
- 		ldr_out=((AD0GDR>>6)&(0X03FF));
- 		while(!(AD1GDR)&(80000000));
- 		lm35_out=((AD1GDR>>6)&(0X03FF));
- 		temp=((lm35_out*100)/1024);
- 		sprintf(a,"temperature=%d\tlm35_out=%d\tldr_out=%d\n",temp,lm35_out,ldr_out);
- 		transmit(a); 
+	else if(control_data=='q')
+	    {  
+ 			AD0CR=0X01200008;
+ 			AD1CR=0X01200008;
+ 			while(!(AD0GDR)&(80000000));
+ 			ldr_out=((AD0GDR>>6)&(0X03FF));
+ 			while(!(AD1GDR)&(80000000));
+ 			lm35_out=((AD1GDR>>6)&(0X03FF));
+ 			temp=((lm35_out*100)/1024);
+ 			hour = HOUR;
+        	min  = MIN; 
+        	sec  = SEC; 
+        	date  = DOM;   
+        	month = MONTH;  
+        	year  = YEAR;
+ 			sprintf(a,"temperature=%d\tlm35_out=%d\tldr_out=%d\ttime:%d:%d:%d\n",temp,lm35_out,ldr_out,hour,min,sec);
+ 			transmit(a);
+ 		} 
  		if(ldr_out<512)
  		{
    			IOSET0=0X80000000;
